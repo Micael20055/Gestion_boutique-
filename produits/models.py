30954767a5680e0e48c1fd099aa68582ab151fff
Micael20055 <alabamicael2@gmail.com>
2025-02-06 23:10:17 +0000

    `Added views for dashboard, products, and sales applications`

--- produits/models.py
+++ produits/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from .models import Categorie
 
 # Create your models here.
+class Categorie(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)    
+
+class Produit(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    quantiteEnStock = models.PositiveIntegerField()
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name='produits')
+
+    def __str__(self):
+        return self.nom
--- ventes/models.py
+++ ventes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from produits.models import Produit
 
 # Create your models here.
+
+
+class Vente(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.CharField(max_length=100)
+    produits = models.ManyToManyField(Produit, related_name='ventes')
+    date = models.DateField(auto_now_add=True)
+    montant = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Vente {self.id} - {self.client}"
